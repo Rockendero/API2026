731a65471c4afe04603dce13fc38ec7dcaf60312
Rockendero <146258545+Rockendero@users.noreply.github.com>
2026-06-16 13:06:19 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 import {config} from 'dotenv'
 config()
 
-export const BD_HOST=process.env.BD_HOST || localhost
+export const BD_HOST = process.env.BD_HOST || 'localhost';
+//export const BD_HOST=process.env.BD_HOST || localhost
 export const BD_DATABASE=process.env.BD_DATABASE || base2026
 export const DB_USER=process.env.DB_USER || root
 export const DB_PASSWORD=process.env.DB_PASSWORD || ''
